319dbe4f08a0fe02492ed8918fe47d71fa9fd36a
manish pareek <manishpareek.3334@gmail.com>
2026-01-06 21:00:16 +0530

    Time: 0 ms (100%), Space: 8.1 MB (91.35%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int count=0;
+        for(char s: stones){
+            for(char j: jewels){
+                if(s==j)
+                    count++;
+            }
+        }
+        return count;
+    }
+};
