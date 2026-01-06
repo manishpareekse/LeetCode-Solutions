9e917d4595ab02e8009401a95602e650d51c0bdd
manish pareek <manishpareek.3334@gmail.com>
2026-01-06 20:54:38 +0530

    Time: 3 ms (33%), Space: 10.4 MB (16.33%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(string str) {
+        string text = "";
+        for (char ch : str) {
+            if (isalnum(ch)) {
+                text += tolower(ch);
+            }
+        }
+        int i = 0;
+        int j = text.size() - 1;
+        while (i < j) {
+            if (text[i] != text[j]) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
