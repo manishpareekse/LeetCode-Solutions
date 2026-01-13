d690ed51ecf861525e8b6b54fe081bde3d476f53
manish pareek <manishpareek.3334@gmail.com>
2026-01-13 09:21:16 +0530

    Time: 0 ms (100%), Space: 29.7 MB (40.81%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        
-        int n=nums.size();
-        k=k%n;
-        reverse(nums.begin(),nums.end()); // pahle pure array ko reverse kiya {6,5,4,3,2,1}
-        reverse(nums.begin(),nums.begin()+k); // fir starting k element ko reverse kiya {4,5,6}
-        reverse(nums.begin()+k,nums.end()); // fir last ke bache huye element ko reverse kiya {1,2,3}
+
+        int n = nums.size();
+        k = k % n;
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };
